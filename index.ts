99cae967eebec4c9f9bfb19ed3e60c57b26d231c
dhruv76 <61657082+dhruv76@users.noreply.github.com>
2026-02-15 20:44:11 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,17 @@ app.use(express.json());
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        handleSIGINT: false,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', 
+            '--disable-gpu'
+        ],
     }
 });
 
